8b51d13c736c5a7ff96af0b53bd69c87c252ea11
Ray Villalobos <ray@planetoftheweb.com>
2021-11-24 12:10:23 -0500

    finished project

---
+++ windi.config.js
@@ -0,0 +1,5 @@
+export default {
+  shortcuts: {
+    'btn': 'self-end cursor-pointer px-6 py-3 border border-transparent rounded-md text-white bg-pink-600 hover:(bg-pink-800 border-4 border-yellow-500)'
+  }
+}
